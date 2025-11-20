b1f2ad7fee8857fa73ace850bf5956613082bb5f
Shirish-Master <shirishshyam15@gmail.com>
2025-11-21 01:54:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Hi, I am Shirish</title>
+      <title>Hi, I am Shirish Shyam</title>
       <style>
         body {
           margin: 0;
